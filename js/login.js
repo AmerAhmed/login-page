a0593183025e27e4f484f3c43ec86016e7ede119
Amer Ahmed <aham21bz@student.ju.se>
2021-09-17 19:31:26 +0200

    Updated js-file contents

--- js/login.js
+++ js/login.js
@@ -3,3 +3,26 @@
 let txtuser = document.querySelector('.txtuser')
 let txtpass = document.querySelector('.txtpass')
 let submit = document.querySelector('.submit')
+
+if (window.localStorage) {
+  // Store values by using key and pair
+  localStorage.setItem('user', 'test')
+  localStorage.setItem('pass', '1234')
+
+  // Get user and pass
+  let user = localStorage.getItem('user')
+  let pass = localStorage.getItem('pass')
+  // Show a message
+  let message = document.querySelector('.message')
+
+  // Check for user and pass by using event click
+  submit.addEventListener('click', () => {
+    if (user == txtuser.value && pass == txtpass.value) {
+      message.innerHTML = 'You have logged in successfully!'
+    } else {
+      message.innerHTML = 'Username or password is invalid!'
+    }
+  })
+} else {
+  console.log('No Supperted')
+}
