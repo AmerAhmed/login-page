b31a2f41f08682131c9b5c9891f00f7625f1416b
Amer Ahmed <aham21bz@student.ju.se>
2021-09-17 19:30:39 +0200

    Adding js-file contents

--- js/login.js
+++ js/login.js
@@ -1,1 +1,5 @@
 'use strict'
+// name, password  an subimt
+let txtuser = document.querySelector('.txtuser')
+let txtpass = document.querySelector('.txtpass')
+let submit = document.querySelector('.submit')
